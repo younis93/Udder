ceecb520bf67895187b7a845abd9e268d27324b4
Younis <younis.only2@gmail.com>
2019-04-26 18:19:04 +0300

    Initial commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mac on 2019-04-26.
+//  Copyright © 2019 HelxApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
